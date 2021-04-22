7f3cee053c39539d23ff145c11fe495ecad83259
Piyush <piyushgargofficial@gmail.com>
2021-04-23 01:31:57 +0530

    Firebase Auth

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Components/Checkout";
+import Login from "./Components/Login";
 
 function App() {
+  useEffect(() => {}, []);
+
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/" exact>
+            <Header />
             <Home />
           </Route>
         </Switch>
